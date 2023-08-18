a34b1742ff939d2080837a420837fd51f887b088
Subhash Mourya <subhash@euronics.com>
2023-08-18 15:40:47 +0530

    Add PUT api for book

--- routes/route.js
+++ routes/route.js
@@ -2,17 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const authorController = require('../controllers/authorController.js');
-
 const loginController = require('../controllers/loginController.js');
+const bookCreateController = require('../controllers/bookCreatController.js');
+const getBooksController = require('../controllers/getBooksController.js');
+const updateBooksController = require('../controllers/updateBookController.js');
+const deleteBooksController = require('../controllers/deleteBookController.js');
+
 
 
 router.post('/register', authorController.createAuthor);
 router.post('/login', loginController.login);
-
-const bookCreateController = require('../controllers/bookCreatController.js');
-
-
 router.post('/books', bookCreateController.createBook);
+router.get('/books', getBooksController.getBooks);
+router.get('/books/:bookId', getBooksController.getBooksById);
+router.put('/books/:bookId', updateBooksController.updateBook);
+router.delete('/books/:bookId', deleteBooksController.deleteBook);
 
 
 
